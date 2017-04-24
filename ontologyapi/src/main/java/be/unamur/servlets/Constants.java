2b903577424c1d83e9860008fcccae11c4bbe623
LAPTOP-ODC88JT0\nutti <nuttinck_alexandre@hotmail.com>
2017-04-24 12:16:51 +0200

    index page

---
+++ ontologyapi/src/main/java/be/unamur/servlets/Constants.java
@@ -0,0 +1,18 @@
+package be.unamur.servlets;
+
+public class Constants {
+	public static final String VIEW_INDEX = "/WEB-INF/index.jsp";
+	public static final String VIEW_REQUEST = "/WEB-INF/request.jsp";
+	
+	public static final String PATH_WS = "/rest";
+	public static final String PATH_ROOT = "/";
+	public static final String PATH_BOWER_COMPONENTS = "/bower_components";
+	public static final String PATH_DIST = "/dist";
+	
+    public static final String CONTROLLER_INDEX = "/index";
+	public static final String CONTROLLER_REQUEST = "/request";
+	
+	public static final String DAL_READ_FACTORY = "dalReadFactory";
+	public static final String DAL_WRITE_FACTORY = "dalWriteFactory";
+
+}
