3399c1143582313a7678c565b91c516bb7af80a8
Axel <axel.halin@student.unamur.be>
2017-04-27 09:21:44 +0200

    Update Entreprise Parser

--- ontologyapi/src/main/java/utils/Parser.java
+++ ontologyapi/src/main/java/utils/Parser.java
@@ -1,31 +1,37 @@
 package utils;
 
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.vocabulary.VCARD;
 
 import ontology.EnterpriseModel;
 
-
+// TODO check if field has value before attempting to write
+// TODO GEOM data
+/**
+ * Parser to instantiate a RDF/XML based on an OWL ontology and an Oracle database content.
+ */
 public class Parser {
-
+	// Name space of our ontology
 	private static final String NS = "http://www.semanticweb.org/Namur/entreprise-ontology#";
+	// Name space of the VCARD ontology (used to describe people)
 	private static final String VCARD = "http://www.w3.org/2006/vcard/ns#";
 
-	
+	/**
+	 * Retrieve the content of a OWL ontology stored in a file.
+	 * 
+	 * @param owlDataFile Name of the OWL ontology file
+	 * @param model Instance of the OWL ontology
+	 */
 	public void putEntreprise(String owlDataFile, OntModel model){
 		model = EnterpriseModel.getOntologyModel(owlDataFile);
 	}
@@ -69,6 +75,7 @@ public class Parser {
 			Class.forName("oracle.jdbc.driver.OracleDriver");    
 			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:technoweb","test","test");  
 			Statement stmt=con.createStatement();  
+			// TODO remove Cafes and Restaurants
 			ResultSet rs=stmt.executeQuery("select * from \"ELYX_DATA\".\"ECON_ENTREPRISES_GEO\"");
 		
 			int i =0;
@@ -78,7 +85,7 @@ public class Parser {
 				//transformToRDF(rs, model);
 				parseEntreprise(rs,model);
 				i++;
-				if(i>6) break;
+				if(i>2) break;
 			}
 			con.close();  
 		} catch(Exception e){
@@ -91,8 +98,8 @@ public class Parser {
 	/**
 	 * Parse the enterprise contained at the current location in the ResultSet to the OntModel.
 	 * 
-	 * @param result
-	 * @param model
+	 * @param result Database row
+	 * @param model OntModel being populated
 	 * @throws SQLException
 	 */
 	private static void parseEntreprise(ResultSet result, OntModel model) throws SQLException{
@@ -107,6 +114,7 @@ public class Parser {
 		parseIsSubjectTVA(entreprise,model,result);
 		parseHasLanguageProperty(entreprise, model, result);
 		parseHasLegalName(entreprise,model,result);
+		parseHasDirector(entreprise, model, result);
 	}
 	
 	private static void parseCafes(){}
@@ -115,25 +123,21 @@ public class Parser {
 	
 	
 	private static void parseHasLegalAdresseProperty(Individual entrepriseInstance, OntModel model, ResultSet result, String owlClass) throws SQLException{
-		if(owlClass.equals("Entreprise")){
-			entrepriseInstance.addProperty(model.getProperty(NS+"hasAdresseLegale"), 
-											model.createResource(model.getResource(NS+"AdresseLegale"))
-																		.addProperty(model.getProperty(VCARD+"street-address"),result.getString("numero")+", "+result.getString("rue"))
-																		.addProperty(model.getProperty(VCARD+"country-name"), "Belgique")
-																		.addProperty(model.getProperty(VCARD+"postal-code"), result.getString("cp"))
-																		.addProperty(model.getProperty(VCARD+"region"), "Namur")
-																		.addProperty(model.getProperty(VCARD+"locality"),result.getString("localite"))
-																		.addProperty(model.getProperty(NS+"hasStreetCode"),result.getString("coderue")));
-		} else{
-			entrepriseInstance.addProperty(model.getProperty(NS+"hasAdresseLegale"), 
-					model.createResource(model.getResource(NS+"AdresseLegale"))
-												.addProperty(model.getProperty(VCARD+"street-address"),result.getString("numero_eta")+", "+result.getString("rue_eta"))
-												.addProperty(model.getProperty(VCARD+"country-name"), "Belgique")
-												.addProperty(model.getProperty(VCARD+"postal-code"), result.getString("cp_eta"))
-												.addProperty(model.getProperty(VCARD+"region"), "Namur")
-												.addProperty(model.getProperty(VCARD+"locality"),result.getString("localite_eta"))
-												.addProperty(model.getProperty(NS+"hasStreetCode"),result.getString("coderue_eta")));			
-		}
+		String numeroField = owlClass.equals("Entreprise") ? "numero" : "numero_eta";
+		String postalCodeField = owlClass.equals("Entreprise") ? "cp" : "cp_eta";
+		String localiteField = owlClass.equals("Entreprise") ? "localite" : "localite_eta";
+		String codeRueField = owlClass.equals("Entreprise") ? "coderue" : "coderue_eta";
+		String streetAddressField = owlClass.equals("Entreprise") ? "rue" : "rue_eta";
+
+		Individual address = model.createIndividual(NS+result.getString(numeroField)+", "+result.getString(streetAddressField), model.getOntClass(NS+"AdresseLegale"));
+		address.addProperty(model.getProperty(VCARD+"street-address"),result.getString(numeroField)+", "+result.getString(streetAddressField))
+				.addProperty(model.getProperty(VCARD+"country-name"), "Belgique")
+				.addProperty(model.getProperty(VCARD+"postal-code"), result.getString(postalCodeField))
+				.addProperty(model.getProperty(VCARD+"region"), "Namur")
+				.addProperty(model.getProperty(VCARD+"locality"),result.getString(localiteField))
+				.addProperty(model.getProperty(NS+"hasStreetCode"),result.getString(codeRueField));
+		
+		entrepriseInstance.addProperty(model.getProperty(NS+"hasAdresseLegale"), address);
 	}
 	
 	private static void parseHasContactProperty(Individual entrepriseInstance, OntModel model, ResultSet result, String owlClass) throws SQLException{
@@ -150,6 +154,7 @@ public class Parser {
 		}
 	}
 	
+	
 	private static void parseHasFormeJuri(Individual entrepriseInstance, OntModel model, ResultSet result) throws SQLException{
 		entrepriseInstance.addProperty(model.getProperty(NS+"hasFormeJuri"), 
 										model.createResource(model.getResource(NS+"SA"))
@@ -159,26 +164,75 @@ public class Parser {
 												.addProperty(model.getProperty(NS+"subscribed"),result.getString("insc_dat")));
 	}
 	
+	/**
+	 * Parse hasTVA property to an Enterprise Individual.
+	 * hasTVA is a NoTVA property mapped to no_tva. It starts with "BE" followed by the TVA number
+	 * 
+	 * @param entrepriseInstance Enterprise Individual being instantiated
+	 * @param model OntModel being populated
+	 * @param result Database row
+	 * @throws SQLException
+	 */
 	private static void parseHasTVA(Individual entrepriseInstance, OntModel model, ResultSet result) throws SQLException{
 		entrepriseInstance.addProperty(model.getProperty(NS+"hasTVA"),
 										model.createResource(model.getResource(NS+"NoTVA"))
 												.addLiteral(model.getProperty(NS+"NoTVA"), "BE"+result.getString("no_tva")));
 	}
 	
+	/**
+	 * Parse isSubjectTVA to the Enterprise Individual.
+	 * isSubjectTVA is a AssujTVA property mapped to assuj_tva. It is either "Yes" or "No"
+	 * 
+	 * @param entrepriseInstance Enterprise Individual being instantiated
+	 * @param model OntModel being populated
+	 * @param result Database row
+	 * @throws SQLException
+	 */
 	private static void parseIsSubjectTVA(Individual entrepriseInstance, OntModel model, ResultSet result) throws SQLException{
 		entrepriseInstance.addProperty(model.getProperty(NS+"isSubjectTVA"),
 										model.createResource(model.getResource(NS+"AssujTVA"))
 												.addLiteral(model.getProperty(NS+"AssujTVA"), result.getString("assuj_tva").equals("Y") ? "Yes" : "No"));
 	}
 	
+	/**
+	 * Parse hasLanguage property to an Individual instance
+	 * hasLanguage is mapped to langue
+	 * 
+	 * @param entrepriseInstance Individual being instantiated
+	 * @param model OntModel being populated
+	 * @param result Database row
+	 * @throws SQLException
+	 */
 	private static void parseHasLanguageProperty(Individual entrepriseInstance, OntModel model, ResultSet result) throws SQLException{
 		entrepriseInstance.addProperty(model.getProperty(VCARD+"hasLanguage"), result.getString("langue").equals("1") ? "FR" : "Unknown...");
 	}
 	
+	/**
+	 * Parse a nomLegal property to a given Entreprise Individual.
+	 * nomLegal is mapped to nom
+	 * 
+	 * @param entrepriseInstance Enterprise Individual being populated
+	 * @param model OntModel being populated
+	 * @param result Database row
+	 * @throws SQLException
+	 */
 	private static void parseHasLegalName(Individual entrepriseInstance, OntModel model, ResultSet result) throws SQLException{
 		entrepriseInstance.addProperty(model.getProperty(NS+"nomLegal"), result.getString("nom"));	
 	}
 	
+	/**
+	 * Parse an Etablissement Individual to the RDF and link it to an Entreprise Individual.
+	 * An Etablissement has:
+	 * 	- Contact infos (@see parseHasContactProperty)
+	 *  - Legal Address (@see parseHasLegalAdresseProperty)
+	 *  - nomLegal mapped to eta_nom
+	 *  - subscribed mapped to insc_dat_eta 
+	 *  
+	 * @param entrepriseInstance Enterprise Individual to which the Etablissement is to be link.
+	 * @param model OntModel being populated
+	 * @param result Database row 
+	 * @throws SQLException
+	 */
 	private static void parseIsEstablished(Individual entrepriseInstance, OntModel model, ResultSet result) throws SQLException{
 		Individual etablissement = model.createIndividual(NS+result.getString("eta_nom"), model.getOntClass(NS+"Etablissement"));
 		parseHasContactProperty(etablissement, model, result, "Etablissement");
@@ -188,12 +242,47 @@ public class Parser {
 		entrepriseInstance.addProperty(model.getProperty(NS+"isEstablished"), etablissement);
 	}
 	
+	/**
+	 * Parse a Directeur Individual to the RDF.
+	 * A Directeur extends a Personne and has:
+	 * 	- hasLanguage mapped to dir_langue
+	 *  - function mapped to dir_fonct_label
+	 *  - genre mapped to dir_sexe
+	 *  - nomDeFamille mapped to dir_nom
+	 *  - prenom mapped to dir_prn
+	 * 
+	 * @param entrepriseInstance Enterprise Individual being populated 
+	 * @param model RDF model being populated
+	 * @param result Database row
+	 * @throws SQLException if the specified field (column name) isn't found.
+	 */
+	private static void parseHasDirector(Individual entrepriseInstance, OntModel model, ResultSet result) throws SQLException{
+		Individual directeur = model.createIndividual(NS+result.getString("dir_prn")+result.getString("dir_nom"), model.getOntClass(NS+"Directeur"));
+		directeur.addProperty(model.getProperty(VCARD+"hasLanguage"), result.getString("dir_langue").equals("1") ? "FR" : "")
+					.addProperty(model.getProperty(NS+"function"), result.getString("dir_fonct_lib"))
+					.addProperty(model.getProperty(NS+"genre"), result.getString("dir_sexe").equals("1")? "Male" : "Female")
+					.addProperty(model.getProperty(NS+"nomDeFamille"), result.getString("dir_nom"))
+					.addProperty(model.getProperty(NS+"prenom"), result.getString("dir_prn"));
+				
+		entrepriseInstance.addProperty(model.getProperty(NS+"hasDirecteur"), directeur);
+	}
 
+	/**
+	 * Export an OntModel to the specified file as RDF/XML. If no file name is specified, the output is redirected to System.out
+	 * 
+	 * @param model OntModel to export
+	 * @param outputFileName Name of the file where to write.
+	 * @throws IOException
+	 */
 	private static void exportModel(OntModel model, String outputFileName) throws IOException{
-		FileWriter out = new FileWriter( outputFileName );
+		FileWriter out = null;
 		try {
-		    //model.write( out, "RDF/XML" );
-			model.write(System.out, "RDF/XML" );
+			if(outputFileName!=null){
+				out = new FileWriter( outputFileName );
+				model.write( out, "RDF/XML" );
+			} else{
+				model.write(System.out, "RDF/XML" );	
+			}
 		}
 		finally {
 		   try {
